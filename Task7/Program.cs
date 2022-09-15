63ce4a11e159829f6871e4ae4bb156ab0cd6acbb
Dmitrii <razuhindm@gmail.com>
2022-09-15 22:38:55 +0300

    Задача на дом №6

---
+++ Task7/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число : ");
+int num1 = Convert.ToInt32(ReadLine()!);
+
+if (num1%2 == 0)
+{
+    Write("Четное");
+}
+else
+{
+    Write("Нечетное");
+}
+
