e7240c4b42c658b9295a93829146c2193b623c74
Dmitrii <razuhindm@gmail.com>
2022-09-15 22:21:29 +0300

    Задача на дом №2

---
+++ Task5/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число а=");
+int num1 = Convert.ToInt32(ReadLine()!);
+Write("Введите число b=");
+int num2 = int.Parse(ReadLine()!);
+if (num1>num2)
+{
+    Write($"Наибольшее из двух введеных чисел: {num1}");
+}
+else
+{
+    Write($"Наибольшее из двух введеных чисел: {num2}");
+}
+
