e478c67cd24c1247b76ca4b0ed0af8746447eb64
Dmitrii <razuhindm@gmail.com>
2022-09-15 22:43:49 +0300

    Задача на дом №8

---
+++ Task8/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+Clear();
+Write("Введите число: ");
+
+int num1=Convert.ToInt32(ReadLine());
+int i=1;
+
+while (i<=num1)
+{
+ if (i%2==0)
+ {
+    Write($"{i} ");
+    i++;
+ }
+ else
+ {
+    i++;
+ }
+}
