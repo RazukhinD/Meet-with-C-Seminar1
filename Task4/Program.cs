655b6e3e025b09eeef9a5cd796c5c6388495147a
Dmitrii <razuhindm@gmail.com>
2022-09-15 22:10:42 +0300

    Оптимизировал код

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -3,7 +3,6 @@
 Clear();
 Write("Введи трехзначное число : ");
 int num1 = Convert.ToInt32(ReadLine()!);
-int num2 = num1 % 100;
-int num3 = num2 % 10;
 
-Write($"Последнее число: {num3}");
+
+Write($"Последнее число: {num1%10}");
